4bffc9ee55435157759b4bd9c120dfac37a11b88
Jongwon Lim <jongwon.lim@cern.ch>
2021-04-12 05:03:30 +0900

    Pull hyu and rename PrivateMC

---
+++ PrivateMC/SMEFT_2l2q_UFO_unitary/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.35
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Thu 24 Oct 2019 15:52:59
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 1,
+                   hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
