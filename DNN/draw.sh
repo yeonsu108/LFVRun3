98b1d762e331eb9d6a608063f4519ca2c736e4ae
Jongwon Lim <jongwon.lim@cern.ch>
2021-12-09 17:44:38 +0900

    updates for DNN

--- DNN/draw.sh
+++ DNN/draw.sh
@@ -1,4 +1,4 @@
-syst=norm
+syst=jecdown
 if [ -z "$1" ]
 then
     echo "No Input Argument"
@@ -6,13 +6,11 @@ else
     cd ${1}/pred_hists/
     rm -rf Run2_${syst}_pred.root
     hadd Run2_${syst}_pred.root 1*/Run*.root
-    #for i in run2 16pre 16post 17 18
-    for i in run2
+    for i in run2 16pre 16post 17 18
+    #for i in run2
     do
-        python ../../drawhists.py -SYS norm -Y $i
-        python ../../drawhists.py -SYS norm -Y $i -L
-    #    python ../drawhists.py -Y $i -S
-    #    python ../drawhists.py -Y $i -L -S
+        python ../../drawhists.py -SYS ${syst} -Y $i
+        python ../../drawhists.py -SYS ${syst} -Y $i -L
     done
     cd ../../
 fi
