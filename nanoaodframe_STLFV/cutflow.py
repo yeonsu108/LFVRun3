8dd69f7f281a629140adce6866fa00a552aa830a
Jongwon Lim <jongwon.lim@cern.ch>
2021-12-09 18:28:28 +0900

    Update cutflow table

--- nanoaodframe_STLFV/cutflow.py
+++ nanoaodframe_STLFV/cutflow.py
@@ -21,7 +21,7 @@ for run in runs:
             flist[run].remove(j)
 
 # !! Check Selection Steps !!
-steps = 4
+steps = 5
 
 # Cross Sections (pb)
 Xsec_dict = {"DYJetsToLL_M-10to50" : 18610.00, 
@@ -142,10 +142,11 @@ def run(run):
                 nevts_entry.append(data.Get("hnevents_cut"+str(0)*(j+1)).GetEntries())
 
         # Rescale for conservation of event yields when btagging SF is applied.
-        prehist = data.Get("hnevents_pglep_cut000")
-        posthist = data.Get("hnevents_cut000")
+        prehist = data.Get("hnevents_pglep_cut0000")
+        posthist = data.Get("hnevents_cut0000")
         rescale = prehist.Integral() / posthist.Integral() if posthist.GetEntries() != 0 else 1.0
-        resflist = [1.0, 1.0, 1.0, rescale, rescale]
+        #resflist = [1.0, 1.0, 1.0, rescale, rescale, rescale]
+        resflist = [1.0, 1.0, 1.0, 1.0, 1.0, rescale]
         data.Close() # rootfile closed
 
         nevts_staterr=np.sqrt(np.array(nevts_entry)).tolist()
@@ -299,7 +300,7 @@ def formatCutflow(cutflow):
     out=np.concatenate((np.array(cut1).flatten(),np.array(cut2).flatten(),np.array(cut3).flatten(),np.array(cut4).flatten()))
     return out
 
-run2cutflow = np.zeros((47,2,5))
+run2cutflow = np.zeros((47,2,steps+1))
 cutflows = []           # Full Lists (shape (48,2,5))
 formattedcutflows = []  # Formatted for Cutflow Table (shape (1,))
 for year in runs:
@@ -319,8 +320,8 @@ formattedcutflows.append(formatCutflow(run2cutflow))
 
 def texScript(run,formattedcutflow):
     h1 = "\\begin{table}[!hp]\n    \\tiny\n    \centering\n"
-    h2 = "    \\renewcommand{\\arraystretch}{1.3}\n    \\begin{tabular}{c|r|r|r|r} \\hline\\hline \n"
-    h3 = "    \\textbf{"+run+" (%s~\\fbinv)} & \\textbf{One muon} & \\textbf{One tau, $m_{\\mu\\tau}>150\GeV$} & \\textbf{nJets$\geq$3} & \\textbf{One b-tagged Jet}\\\\ \hline \n"%(lumi)
+    h2 = "    \\renewcommand{\\arraystretch}{1.3}\n    \\begin{tabular}{c|r|r|r|r|r} \\hline\\hline \n"
+    h3 = "    \\textbf{"+run+" (%s~\\fbinv)} & \\textbf{One muon} & & \\textbf{One tau} & \\textbf{$m_{\\mu\\tau}>150\GeV$, OS} & \\textbf{nJets$\geq$3} & \\textbf{One b-tagged Jet} \\\\ \hline \n"%(lumi)
     s = "    LFV ST $tc\\mu\\tau$  " + " & {} $\\pm$ {}"*steps + " \\\\ \n\
     LFV ST $tu\\mu\\tau$  " + " & {} $\\pm$ {}"*steps + " \\\\ \n\
     LFV TT $tc\\mu\\tau$  " + " & {} $\\pm$ {}"*steps + " \\\\ \n\
@@ -399,7 +400,7 @@ bin             bin1
 observation     {0:<12}
 ----------------------------------------------------------------------------------------------------
 bin             bin1        bin1        bin1        bin1        bin1        bin1        bin1
-process         lfv_st      lfv_tt      tt          dy          st          wjet        others
+process         lfv_st      lfv_tt      TT          DY          ST          Wjet        Oth
 process         0           -1          1           2           3           4           5
 rate            {1:<12.3f}{2:<12.3f}{3:<12.3f}{4:<12.3f}{5:<12.3f}{6:<12.3f}{7:<12.3f}
 ----------------------------------------------------------------------------------------------------
