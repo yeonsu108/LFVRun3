8eac7b1e991c434913bcd802ef251a0cbc61a3d1
Jiwon Park <minerva1993@gmail.com>
2022-10-27 10:38:31 +0900

    loop for card writer

--- DNN/plot_run2.py
+++ DNN/plot_run2.py
@@ -12,12 +12,13 @@ common_syst_list = ['pu', 'btaglf', 'btaghf', 'btaglfstat1',
                     'btaglfstat2', 'btaghfstat1', 'btaghfstat2', 'btagcferr1', 'btagcferr2',
                     'jesAbsolute', 'jesBBEC1', 'jesEC2', 'jesFlavorQCD', 'jesRelativeBal']
 syst = ['jesAbsoluteyear', 'jesBBEC1year', 'jesEC2year', 'jesRelativeSampleyear']
+
+years = {'16pre':19500, '16post':16800, '17':41529, '18':59741}
+
 for sy in syst:
   if 'year' in sy:
-    common_syst_list.append(sy.replace('year', '16pre'))
-    common_syst_list.append(sy.replace('year', '16post'))
-    common_syst_list.append(sy.replace('year', '17'))
-    common_syst_list.append(sy.replace('year', '18'))
+    for year in years.keys():
+      common_syst_list.append(sy.replace('year', year))
   else: common_syst_list.append(sy)
 
 reco_str = 'rerun_CHaug22/'
@@ -38,74 +39,40 @@ for item in common_syst_list:
 for ch in chs:
   string_for_files = ''
   dest_path = reco_str.replace('CH', ch)
-  #Firstly, merge file list + scale
-  with open(config_path + 'files_2016pre.yml') as f:
-    lines = f.readlines()
-    skip_signal = False
-    for line in lines:
-      #if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist' in line: skip_signal = False
-      if 'LFV' in line: skip_signal = True
-      if 'hist_QCD' in line: skip_signal = True
-      if 'hist' in line:
-        line = line[0] + dest_path+ '16pre_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(19500/137570.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']):
-        if 'group' in line and not any(i in line for i in groups): string_for_files += '  group: Gother \n'
-        else: string_for_files += line
 
-  with open(config_path + 'files_2016post.yml') as f:
-    lines = f.readlines()
-    skip_signal = False
-    for line in lines:
-      #if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist' in line: skip_signal = False
-      if 'LFV' in line: skip_signal = True
-      if 'hist_QCD' in line: skip_signal = True
-      if 'hist' in line:
-        line = line[0] + dest_path + '16post_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(16800/137570.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']):
-        if 'group' in line and not any(i in line for i in groups): string_for_files += '  group: Gother \n'
-        else: string_for_files += line
+  for year, lumi in years.items():
+    #Firstly, merge file list + scale
+    with open(config_path + 'files_' + year + '.yml') as f:
+      lines = f.readlines()
+      skip_signal = False
+      for line in lines:
+        #if '#' in line[0]: line = line[1:]
+        if skip_signal and 'hist' in line: skip_signal = False
+        if 'LFV' in line: skip_signal = True
+        if 'hist_QCD' in line: skip_signal = True
+        if '#' in line[0]: skip_signal = True
+        if 'hist' in line:
+          line = line[0] + dest_path+ year + '_postprocess/' + line[1:]
+          if not any(i in line for i in ['LFV', 'Run1']):
+            line += '  scale: ' + str(int(lumi)/137570.0) + '\n'
+        if not skip_signal and not any(i in line for i in ['yields-group']):
+          if 'group' in line and not any(i in line for i in groups): string_for_files += '  group: Gother \n'
+          else: string_for_files += line
 
-  with open(config_path + 'files_2017.yml') as f:
-    lines = f.readlines()
-    skip_signal = False
-    for line in lines:
-      #if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist' in line: skip_signal = False
-      if 'LFV' in line: skip_signal = True
-      if 'hist_QCD' in line: skip_signal = True
-      if 'hist' in line:
-        line = line[0] + dest_path + '17_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(41529/137570.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']):
-        if 'group' in line and not any(i in line for i in groups): string_for_files += '  group: Gother \n'
-        else: string_for_files += line
+    file_syst = ''
+    with open(config_path + 'config_' + year + '.yml') as f:
+      lines = f.readlines()
+      for line in lines:
+        if 'type' in line:
+          if 'const' in line:
+            file_syst += line[:line.find(':')] + '_' + year + line[line.find(':'):line.find('hist')] + year + '_postprocess/' + line[line.find('hist'):]
+          elif 'shape' in line:
+            file_syst += line[:line.find('hist')] + year + '_postprocess/' + line[line.find('hist'):]
 
-  with open(config_path + 'files_2018.yml') as f:
-    lines = f.readlines()
-    skip_signal = False
-    for line in lines:
-      #if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist' in line: skip_signal = False
-      if 'LFV' in line: skip_signal = True
-      if 'hist_QCD' in line: skip_signal = True
-      if 'hist' in line:
-        line = line[0] + dest_path + '18_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(59741/137570.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']):
-        if 'group' in line and not any(i in line for i in groups): string_for_files += '  group: Gother \n'
-        else: string_for_files += line
 
   with open(config_path + 'files_Run2.yml', 'w+') as fnew:
     print("""
-'{0}/Run2/hist_ST_LFV_TCMuTau_Vector.root':
+'{0}Run2/hist_ST_LFV_TCMuTau_Vector.root':
   type: signal
   pretty-name: 'LFVSTcv'
   cross-section: 0.0368
@@ -113,7 +80,7 @@ for ch in chs:
   group: GLFVSTcv
   order: 1
 
-'{0}/Run2/hist_ST_LFV_TUMuTau_Vector.root':
+'{0}Run2/hist_ST_LFV_TUMuTau_Vector.root':
   type: signal
   pretty-name: 'LFVSTuv'
   cross-section: 0.393
@@ -121,7 +88,7 @@ for ch in chs:
   group: GLFVSTuv
   order: 2
 
-'{0}/Run2/hist_TT_LFV_TToCMuTau_Vector.root':
+'{0}Run2/hist_TT_LFV_TToCMuTau_Vector.root':
   type: signal
   pretty-name: 'LFVTTcv'
   cross-section: 0.0215
@@ -129,7 +96,7 @@ for ch in chs:
   group: GLFVTTcv
   order: 3
 
-'{0}/Run2/hist_TT_LFV_TToUMuTau_Vector.root':
+'{0}Run2/hist_TT_LFV_TToUMuTau_Vector.root':
   type: signal
   pretty-name: 'LFVTTuv'
   cross-section: 0.0215
@@ -139,42 +106,6 @@ for ch in chs:
     """.format(dest_path), file=fnew)
     fnew.write(string_for_files)
 
-  file_syst = ''
-  with open(config_path + 'config_2016pre.yml') as f:
-    lines = f.readlines()
-    for line in lines:
-      if 'type' in line:
-        if 'const' in line:
-          file_syst += line[:line.find(':')] + '_17' + line[line.find(':'):line.find('hist')] + '16pre_postprocess/' + line[line.find('hist'):]
-        elif 'shape' in line:
-          file_syst += line[:line.find('hist')] + '16pre_postprocess/' + line[line.find('hist'):]
-
-  with open(config_path + 'config_2016post.yml') as f:
-    lines = f.readlines()
-    for line in lines:
-      if 'type' in line:
-        if 'const' in line:
-          file_syst += line[:line.find(':')] + '_17' + line[line.find(':'):line.find('hist')] + '16post_postprocess/' + line[line.find('hist'):]
-        elif 'shape' in line:
-          file_syst += line[:line.find('hist')] + '16post_postprocess/' + line[line.find('hist'):]
-
-  with open(config_path + 'config_2017.yml') as f:
-    lines = f.readlines()
-    for line in lines:
-      if 'type' in line:
-        if 'const' in line:
-          file_syst += line[:line.find(':')] + '_17' + line[line.find(':'):line.find('hist')] + '17_postprocess/' + line[line.find('hist'):]
-        elif 'shape' in line:
-          file_syst += line[:line.find('hist')] + '17_postprocess' + line[line.find('hist'):]
-
-  with open(config_path + 'config_2018.yml') as f:
-    lines = f.readlines()
-    for line in lines:
-      if 'type' in line:
-        if 'const' in line:
-          file_syst += line[:line.find(':')] + '_18' + line[line.find(':'):line.find('hist')] + '18_postprocess/' + line[line.find('hist'):]
-        elif 'shape' in line:
-          file_syst += line[:line.find('hist')] + '18_postprocess/' + line[line.find('hist'):]
 
   with open(config_path + 'template_Run2.yml') as f:
     lines = f.readlines()
@@ -182,8 +113,6 @@ for ch in chs:
       for line in lines: f1.write(line)
       f1.write(common_syst)
       f1.write(file_syst)
-      #if scenario == 'STFCNC': f1.write("\nplots:\n  include: ['histos_control.yml', 'histos_" + scenario.lower() + ".yml']\n")
-      #else: f1.write("\nplots:\n  include: ['histos_" + scenario.lower() + ".yml']\n")
       f1.write("\nplots:\n  include: ['histos_dnn.yml']\n")
 
   call(['../plotIt/plotIt', '-o ' + dest_path + '/figures', config_path + 'config_Run2.yml'], shell=False)
@@ -191,53 +120,20 @@ for ch in chs:
 
   #For QCD
   string_for_qcd = ''
-  with open(config_path + 'files_2016pre.yml') as f:
-    lines = f.readlines()
-    skip_signal = True
-    for line in lines:
-      if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist_QCD' in line: skip_signal = False
-      if 'hist_QCD' in line:
-        line = line[0] + '16pre_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(19500/101270.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']): string_for_qcd += line
+  for year, lumi in years.items():
+    with open(config_path + 'files_' + year + '.yml') as f:
+      lines = f.readlines()
+      skip_signal = True
+      for line in lines:
+        #if '#' in line[0]: line = line[1:]
+        if '#' in line[0]: skip_signal = True
+        if skip_signal and 'hist_QCD' in line: skip_signal = False
+        if 'hist_QCD' in line:
+          line = line[0] + year + '_postprocess/' + line[1:]
+          if not any(i in line for i in ['LFV', 'Run1']):
+            line += '  scale: ' + str(int(lumi)/137570.0) + '\n'
+        if not skip_signal and not any(i in line for i in ['yields-group']): string_for_qcd += line
 
-  with open(config_path + 'files_2016post.yml') as f:
-    lines = f.readlines()
-    skip_signal = True
-    for line in lines:
-      if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist_QCD' in line: skip_signal = False
-      if 'hist_QCD' in line:
-        line = line[0] + '16post_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(16800/101270.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']): string_for_qcd += line
-
-  with open(config_path + 'files_2017.yml') as f:
-    lines = f.readlines()
-    skip_signal = True
-    for line in lines:
-      if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist_QCD' in line: skip_signal = False
-      if 'hist_QCD' in line:
-        line = line[0] + '17_postprocess' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(41529/101270.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']): string_for_qcd += line
-
-  with open(config_path + 'files_2018.yml') as f:
-    lines = f.readlines()
-    skip_signal = True
-    for line in lines:
-      if '#' in line[0]: line = line[1:]
-      if skip_signal and 'hist_QCD' in line: skip_signal = False
-      if 'hist_QCD' in line:
-        line = line[0] + '18_postprocess/' + line[1:]
-        if not any(i in line for i in ['LFV', 'Run1']):
-          line += '  scale: ' + str(59741/101270.0) + '\n'
-      if not skip_signal and not any(i in line for i in ['yields-group']): string_for_qcd += line
 
   with open(config_path + 'files_Run2.yml', 'a') as fnew:
     fnew.write(string_for_qcd)
@@ -248,8 +144,6 @@ for ch in chs:
       for line in lines: f1.write(line)
       f1.write(common_syst)
       f1.write(file_syst)
-      #if scenario == 'STFCNC': f1.write("\nplots:\n  include: ['histos_control_qcd.yml', 'histos_" + scenario.lower() + "_qcd.yml']\n")
-      #else: f1.write("\nplots:\n  include: ['histos_" + scenario.lower() + "_qcd.yml']\n")
       f1.write("\nplots:\n  include: ['histos_dnn.yml']\n")
 
   #call(['../plotIt/plotIt', '-o ' + dest_path + '/figures/qcd', config_path + 'config_Run2.yml'], shell=False)
