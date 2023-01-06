b3ca3105036d85cfca30963a971921a2bab4816b
Jiwon Park <jiwonpark1993@gmail.com>
2023-01-06 14:11:50 +0900

    indent, syst for signal stack

--- nanoaodframe/stack_signals.py
+++ nanoaodframe/stack_signals.py
@@ -16,62 +16,79 @@ input = args.input
 lumi_dict = {'2016pre': 19502, '2016post': 16812, '2017': 41480, '2018':59832}#16: 36314, run2:137625
 file_names = collections.OrderedDict()
 
+syst = ["","jesAbsoluteup","jesAbsolutedown", "jesAbsolute_ERAup", "jesAbsolute_ERAdown",
+        "jesBBEC1up", "jesBBEC1down", "jesBBEC1_ERAup", "jesBBEC1_ERAdown",
+        "jesFlavorQCDup", "jesFlavorQCDdown", "jesRelativeBalup", "jesRelativeBaldown",
+        "jesRelativeSample_ERAup", "jesRelativeSample_ERAdown",
+        "jerup", "jerdown", "tesup", "tesdown"]
+syst2 = []
+
+for sy in syst:
+    if 'ERA' in sy:
+        for key in lumi_dict.keys():
+            syst2.append('__' + sy.replace('ERA', key[:4]))
+    elif sy == "": syst2.append(sy)
+    else:          syst2.append('__' + sy)
+
+syst2 = list(set(syst2))
+
 if not os.path.exists(input + "/Run2"):
-  try: os.makedirs(input + "/Run2")
-  except: pass
+    try: os.makedirs(input + "/Run2")
+    except: pass
 
 def store_file(it):
-  path = it[0]
-  file_name = it[1]
-  for f in file_name:
-    print(os.path.join(path, f[:f.rfind('_')] + '.root'))
-    ftmp = TFile.Open(os.path.join(path, f[:f.rfind('_')] + '.root'), 'READ')
+    path = it[0]
+    file_name = it[1]
+    for f in file_name:
+        print(os.path.join(path, f[:f.rfind('_')] + '.root'))
+        ftmp = TFile.Open(os.path.join(path, f[:f.rfind('_')] + '.root'), 'READ')
 
-    hist_names = [x.GetName() for x in ftmp.GetListOfKeys()]
-    hist_names = list(dict.fromkeys(hist_names)) #remove duplicates from more than one instances 
-    hist_names[:] = [item for item in hist_names if item not in ['h_counter_nocut']]
-    hist_names.sort()
+        hist_names = [x.GetName() for x in ftmp.GetListOfKeys()]
+        hist_names = list(dict.fromkeys(hist_names)) #remove duplicates from more than one instances 
+        hist_names[:] = [item for item in hist_names if item not in ['hcounter']]
+        hist_names.sort()
 
-    ntmp = ftmp.Get("hcounter").GetBinContent(2)
+        ntmp = ftmp.Get("hcounter").GetBinContent(2)
 
-    dest_name = path.split('/')[0] + '/Run2/' + f
-    dest = TFile.Open(dest_name, 'RECREATE')
+        dest_name = path.split('/')[0] + '/Run2/' + f
+        dest = TFile.Open(dest_name, 'RECREATE')
 
-    print('Writing scaled histogram to ' + dest_name)
+        print('Writing scaled histogram to ' + dest_name)
 
-    for hist in hist_names:
-      htmp = ftmp.Get(hist)
+        for hist in hist_names:
+            htmp = ftmp.Get(hist)
 
-      if "hcounter" in hist:
-        htmp.Write()
-      else:
-        htmp.Scale(lumi_dict[path.split('/')[1].split('_')[0]]/ntmp)
-        dest.cd()
-        htmp.Write()
-    dest.Write()
-    dest.Close()
+            if "hcounter" in hist:
+                htmp.Write()
+            else:
+                htmp.Scale(lumi_dict[path.split('/')[1].split('_')[0]]/ntmp)
+                dest.cd()
+                htmp.Write()
+        dest.Write()
+        dest.Close()
 
 
 if __name__ == '__main__':
 
-  for era in ['2016pre', '2016post', '2017', '2018']:
-     dir_path = os.path.join(input, era+'_postprocess')
-     dirs = os.listdir(dir_path)
-     print("POST process path: " , dir_path)
-     dirs[:] = [item.replace('.root', '_' + era + '.root') for item in dirs if any(i in item for i in ['LFV'])]
-     print("EDITED DIRS: " , dirs)
-     file_names[dir_path] = dirs
+    for era in ['2016pre', '2016post', '2017', '2018']:
+        dir_path = os.path.join(input, era+'_postprocess')
+        dirs = os.listdir(dir_path)
+        print("POST process path: " , dir_path)
+        dirs[:] = [item.replace('.root', '_' + era + '.root') for item in dirs if any(i in item for i in ['LFV'])]
+        print("EDITED DIRS: " , dirs)
+        file_names[dir_path] = dirs
 
-  pool = multiprocessing.Pool(1)
-  pool.map(store_file, file_names.items())
-  pool.close()
-  pool.join()
+    pool = multiprocessing.Pool(20)
+    pool.map(store_file, file_names.items())
+    pool.close()
+    pool.join()
 
-  chs = ['ST_LFV_TCMuTau_Scalar', 'ST_LFV_TCMuTau_Tensor', 'ST_LFV_TCMuTau_Vector',
-         'ST_LFV_TUMuTau_Scalar', 'ST_LFV_TUMuTau_Tensor', 'ST_LFV_TUMuTau_Vector',
-         'TT_LFV_TCMuTau_Scalar', 'TT_LFV_TCMuTau_Tensor', 'TT_LFV_TCMuTau_Vector',
-         'TT_LFV_TUMuTau_Scalar', 'TT_LFV_TUMuTau_Tensor', 'TT_LFV_TUMuTau_Vector']
+    chs = ['ST_LFV_TCMuTau_Scalar', 'ST_LFV_TCMuTau_Tensor', 'ST_LFV_TCMuTau_Vector',
+           'ST_LFV_TUMuTau_Scalar', 'ST_LFV_TUMuTau_Tensor', 'ST_LFV_TUMuTau_Vector',
+           'TT_LFV_TCMuTau_Scalar', 'TT_LFV_TCMuTau_Tensor', 'TT_LFV_TCMuTau_Vector',
+           'TT_LFV_TUMuTau_Scalar', 'TT_LFV_TUMuTau_Tensor', 'TT_LFV_TUMuTau_Vector']
 
-  for ch in chs:
-    print(input + '/Run2/hist_' + ch + '_201*.root')
-    check_call(['hadd','-f', input + '/Run2/hist_' + ch + '.root'] +  glob.glob(input + '/Run2/hist_' + ch + '_201*.root'))
+    for ch in chs:
+        for syst in syst2:
+            print(input + '/Run2/hist_' + ch + syst + '_201*.root')
+            check_call(['hadd','-f', input + '/Run2/hist_' + ch + syst + '.root'] +  glob.glob(input + '/Run2/hist_' + ch + syst + '_201*.root'))
