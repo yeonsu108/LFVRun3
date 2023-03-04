79fd9e1f318367f3d5648bed58c1ddb9cbfd9054
Jiwon Park <jiwonpark1993@gmail.com>
2023-03-04 21:40:57 +0900

    PV selection

--- nanoaodframe/src/SkimEvents.cpp
+++ nanoaodframe/src/SkimEvents.cpp
@@ -25,11 +25,11 @@ void SkimEvents::defineCuts()
   // These will be passed to Filter method of RDF
   // check for good json event is defined earlier
   if (_year.find("16") != std::string::npos) {
-      addCuts("Flag_filter && (HLT_IsoMu24 || HLT_IsoTkMu24) && nmuonpass == 1","0");
+      addCuts("Flag_filter && (HLT_IsoMu24 || HLT_IsoTkMu24) && nmuonpass == 1 && PV_npvsGood > 0","0");
   } else if (_year.find("17") != std::string::npos) {
-      addCuts("Flag_filter && HLT_IsoMu27 && nmuonpass == 1","0");
+      addCuts("Flag_filter && HLT_IsoMu27 && nmuonpass == 1 && PV_npvsGood > 0","0");
   } else if (_year.find("18") != std::string::npos) {
-      addCuts("Flag_filter && HLT_IsoMu24 && nmuonpass == 1","0");
+      addCuts("Flag_filter && HLT_IsoMu24 && nmuonpass == 1 && PV_npvsGood > 0","0");
   }
   //Prescription to fill up WJets HT = 0-100
   if (_isHTstitching)
