a6e732d108c7b7443aec46e2a6a0ce82fd009241
Jiwon Park <jiwonpark1993@gmail.com>
2024-04-26 17:03:24 +0900

    postproc2 to DNN

---
+++ DNN/postprocess_2.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import ROOT
+from ROOT import *
+import numpy as np
+import optparse
+from optparse import OptionParser
+
+# Set Runs
+years = ['2016pre', '2016post', '2017', '2018']
+
+parser = OptionParser(usage="%prog [options]")
+parser.add_option("-I", "--input",  dest="input", type="string", default="", help="Input file name")
+(options, args) = parser.parse_args()
+base_path = options.input
+
+def collect_systhists(outfile, fname, year):
+    fileList_wt_syst = [i for i in os.listdir(inpath) if fname in i]
+    for sysfile in fileList_wt_syst:
+        tmpf = TFile.Open(os.path.join(inpath, sysfile), 'READ')
+        hlists = [ h.GetName() for h in tmpf.GetListOfKeys() if any(i in h.GetName() for i in ['dnn_pred_S5', 'counter']) ]
+        for histname in hlists:
+            tmpf.cd()
+            tmphist = tmpf.Get(histname)
+            tmphist.SetDirectory(0)
+            outfile.cd()
+            if not "__" in sysfile:
+                tmphist.Write()
+            if "__" in sysfile:
+                syst = sysfile.split("__")[1].split(".root")[0] 
+                tmphist.Write(histname+"__"+syst)
+        tmpf.Close()
+    return
+
+for year in years:
+
+    inpath  = os.path.join(base_path, year + '_postprocess')
+    outpath = os.path.join(base_path, year + '_postprocess_2')
+    print("Directory created: ", outpath)
+    os.makedirs(outpath, exist_ok=True)
+
+    file_list = [i.replace('.root','') for i in os.listdir(inpath) if '.root' in i and not '__' in i]
+    for fname in file_list:
+        outfname = fname + ".root"
+        print("Saving histograms at {}/{}".format(outpath, outfname))
+        outfile = TFile.Open(os.path.join(outpath, outfname), 'RECREATE')
+        collect_systhists(outfile, fname, year)
+        outfile.Close()
